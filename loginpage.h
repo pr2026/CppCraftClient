2516c13e32764f7d4176aad0503715a3f3e39d6e
sonya <yar.sofya13@gmail.com>
2026-03-01 20:08:41 +0300

    Add registration window

---
+++ loginpage.h
@@ -0,0 +1,26 @@
+#ifndef LOGINPAGE_H
+#define LOGINPAGE_H
+
+#include <QWidget>
+
+namespace Ui {
+class LoginPage;
+}
+
+class LoginPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit LoginPage(QWidget *parent = nullptr);
+    ~LoginPage();
+
+private:
+    Ui::LoginPage *ui;
+
+private slots:
+    void clear_clicked();
+    void ok_clicked();
+};
+
+#endif // LOGINPAGE_H
